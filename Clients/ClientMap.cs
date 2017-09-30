f6a27d96bf4d9def78042437ec7edc834ac62b34
OlivierIssaTherrien <oli_sport94@hotmail.com>
2017-09-29 21:06:45 -0400

    regler probleme

--- Clients/ClientMap.cs
+++ Clients/ClientMap.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentNHibernate.Mapping;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using FluentNHibernate.Mapping;
 
-namespace Facturio.Clients
-{
-    public class ClientMap : ClassMap<Client>
-    {
-        public ClientMap()
-        {
-            Table("clients");
+//namespace Facturio.Clients
+//{
+//    public class ClientMap : ClassMap<Client>
+//    {
+//        public ClientMap()
+//        {
+//            Table("clients");
 
-            LazyLoad();
+//            LazyLoad();
 
-            Id(x => x.IdClient)
-                .Column("idClient")
-                .CustomType<int>()
-                .Access.Property()
-                .CustomSqlType("INTEGER")
-                .NotNullable()
-                .GeneratedBy.Identity();
+//            Id(x => x.IdClient)
+//                .Column("idClient")
+//                .CustomType<int>()
+//                .Access.Property()
+//                .CustomSqlType("INTEGER")
+//                .NotNullable()
+//                .GeneratedBy.Identity();
 
-            Map(x)
+           
 
-        }
+//        }
 
 
-    }
-}
+//    }
+//}
--- Clients/HibernateClientService.cs
+++ Clients/HibernateClientService.cs
@@ -1,59 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NHibernate;
-using NHibernate.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using NHibernate;
+//using NHibernate.Linq;
 
-namespace Facturio.Clients
-{
-    public static class HibernateClientService
-    {
-        private static ISession session = NHibernateConnexion.OpenSession();
+//namespace Facturio.Clients
+//{
+//    public static class HibernateClientService
+//    {
+//        private static ISession session = NHibernateConnexion.OpenSession();
 
-        public static List<Client> RetrieveAll()
-        {
-            return session.Query<Client>().ToList();
-        }
+//        public static List<Client> RetrieveAll()
+//        {
+//            return session.Query<Client>().ToList();
+//        }
 
-        public static List<Client> Retrieve(int idClient)
-        {
-            var client = session.Query<Client>().AsQueryable();
+//        public static List<Client> Retrieve(int idClient)
+//        {
+//            var client = session.Query<Client>().AsQueryable();
 
-            var result = from c in Clients
-                         where c.IdClient = idClient
-                         select c;
+//            var result = from c in Clients
+//                         where c.IdClient = idClient
+//                         select c;
 
-            return result.ToList();
-        }
+//            return result.ToList();
+//        }
 
-        public static Create(ClientMap client)
-        {
-            using (var transaction = session.BeginTransaction())
-            {
-                session.Save(client);
-                transaction.Commit();
-            }
+//        public static void Create(ClientMap client)
+//        {
+//            using (var transaction = session.BeginTransaction())
+//            {
+//                session.Save(client);
+//                transaction.Commit();
+//            }
 
-        }
+//        }
 
-        public static void Update(Client client)
-        {
-            using (var transaction = session.BeginTransaction())
-            {
-                session.Update(client);
-                transaction.Commit();
-            }
-        }
+//        public static void Update(Client client)
+//        {
+//            using (var transaction = session.BeginTransaction())
+//            {
+//                session.Update(client);
+//                transaction.Commit();
+//            }
+//        }
 
-        public static void Delete(Client client)
-        {
-            using (var transaction = session.BeginTransaction())
-            {
-                session.Delete(client);
-                transaction.Commit();
-            }
-        }
-    }
-}
+//        public static void Delete(Client client)
+//        {
+//            using (var transaction = session.BeginTransaction())
+//            {
+//                session.Delete(client);
+//                transaction.Commit();
+//            }
+//        }
+//    }
+//}
