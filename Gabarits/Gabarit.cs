8079142b7502cf37cd1492a1f4ed1acd5e95645c
Fredzx <1305906@cstj.qc.ca>
2017-10-03 18:30:55 -0400

    Ajout de propriétés dans la classe Gabarit.cs

--- Gabarits/Gabarit.cs
+++ Gabarits/Gabarit.cs
@@ -2,6 +2,18 @@
 {
     public class Gabarit
     {
-        public string Test { get; set; }
+        private readonly uint idGabarit;
+
+        public uint PrenomClient { get; set; }
+        public uint NomClient { get; set; }
+        public uint Quantite { get; set; }
+        public uint Prix { get; set; }
+        public uint Description { get; set; }
+        public uint AdresseClient { get; set; }
+        public uint CodePostalClient { get; set; }
+        public uint Escompte { get; set; }
+        public uint CritereLibre { get; set; }
+        public uint NombreHeures { get; set; }
+        public uint TauxHoraire { get; set; }
     }
 }
