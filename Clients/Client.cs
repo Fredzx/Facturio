31639ad46ea964e01246fe1ddbc19d9816c568b8
Miguel Dupuis <1547201@cstj.qc.ca>
2017-10-04 16:37:53 -0400

    Aller chier

--- Clients/Client.cs
+++ Clients/Client.cs
@@ -3,6 +3,7 @@
 
     public class Client
     {
+        public int IdClient { get; set; }
         public string Adresse { get; set; }
         public string NoClient { get; set; }
         public string Prenom { get; set; }
@@ -10,17 +11,22 @@
         public string Sexe { get; set; }
         public string Telephone { get; set; }
         public string Description { get; set; }
+        public string CodePostal { get; set; }
+        public int Rang { get; set; }
 
-        public Client(string prenom, string adresse, string nom, string sexe, 
-                      string telephone, string noClient, string description)
+        public Client(string prenom,    string adresse,  string nom, string sexe, 
+                      string telephone, string description,
+                      string codePostal, int rang)
         {
             Adresse = adresse;
-            NoClient = noClient;
+           // NoClient = noClient;
             Prenom = prenom;
             Nom = nom;
             Telephone = telephone;
             Sexe = sexe;
             Description = description;
+            Rang = rang;
+            CodePostal = codePostal;
         }
     }
 }
