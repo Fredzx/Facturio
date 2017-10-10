32e0320ca2d4a91d7020ee40c436877854410a03
Fredzx <1305906@cstj.qc.ca>
2017-10-10 18:11:45 -0400

    Un beau Gabarit selecteur

--- Gabarits/ViewModel/ListeGabaritsViewModel.cs
+++ Gabarits/ViewModel/ListeGabaritsViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.ObjectModel;
 using Facturio.Base;
-using System.Windows.Input;
-using System.Windows;
 
 namespace Facturio.Gabarits.ViewModel
 {
@@ -10,33 +8,16 @@ namespace Facturio.Gabarits.ViewModel
         public ObservableCollection<Gabarit> LstGabarits { get; set; }
         public Gabarit SelectionCourante { get; set; }
 
-        public ICommand Selection { get; set; }
-
         public ListeGabaritsViewModel()
         {
             LstGabarits = new ObservableCollection<Gabarit>()
             {
                 new Gabarit() { Titre = "Titre #1" },
                 new Gabarit() { Titre = "Titre #2" },
-                new Gabarit() { Titre = "Titre #3" },
-                new Gabarit() { Titre = "Titre #4" },
-                new Gabarit() { Titre = "Titre #5" }
+                new Gabarit() { Titre = "Titre #3" }
             };
 
-            Selection = new RelayCommand(PeutSelectionner, Selectionne);
-
             SelectionCourante = LstGabarits[1];
-        }
-
-        public bool PeutSelectionner(object parameter) => true;
-
-        public void Selectionne(object parameter)
-        {
-            if (parameter == null)
-                return;
-
-            SelectionCourante = parameter as Gabarit;
-            MessageBox.Show(SelectionCourante.Titre);
         }
     }
 }
