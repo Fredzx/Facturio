aaad651002ce218925974863fef6482e1ac310a7
Fredzx <1305906@cstj.qc.ca>
2017-10-28 14:37:05 -0400

    Ajout du fichier HibernateTypeCritereService.cs

---
+++ Criteres/HibernateTypeCritereService.cs
@@ -0,0 +1,55 @@
+﻿using NHibernate;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Linq;
+
+namespace Facturio.Criteres
+{
+    class HibernateTypeCritereService
+    {
+        private static ISession session = NHibernateConnexion.OpenSession();
+
+        public static List<TypeCritere> RetrieveAll()
+        {
+            return session.Query<TypeCritere>().ToList();
+        }
+
+        public static List<TypeCritere> Retrieve(int idTypeCritere)
+        {
+            var typeCritere = session.Query<TypeCritere>().AsQueryable();
+
+            var result = from g in typeCritere
+                where g.Id == idTypeCritere
+                select g;
+
+            return result.ToList();
+        }
+
+        public static void Create(TypeCritere typeCritere)
+        {
+            using (var transaction = session.BeginTransaction())
+            {
+                session.Save(typeCritere);
+                transaction.Commit();
+            }
+        }
+
+        public static void Update(TypeCritere typeCritere)
+        {
+            using (var transaction = session.BeginTransaction())
+            {
+                session.Update(typeCritere);
+                transaction.Commit();
+            }
+        }
+
+        public static void Delete(TypeCritere typeCritere)
+        {
+            using (var transaction = session.BeginTransaction())
+            {
+                session.Delete(typeCritere);
+                transaction.Commit();
+            }
+        }
+    }
+}
