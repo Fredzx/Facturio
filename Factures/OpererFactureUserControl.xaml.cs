8e038629dc5c23068a20bcc630499a356600959f
bazzou789 <bazzou789@gmail.com>
2017-11-24 18:13:47 -0500

    J'essaie de faire de quoi...

--- Factures/OpererFactureUserControl.xaml.cs
+++ Factures/OpererFactureUserControl.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Facturio.Factures
 {
@@ -19,10 +22,30 @@ namespace Facturio.Factures
             {
                 if (gc.EstUtilise)
                 {
-                    var dtgTxtCol = new DataGridTextColumn { Header = gc.Critere.Titre };
+                    var dtgTxtCol = new DataGridTextColumn { Header = gc.Critere.Titre};
+                    var titre = RemoveDiacritics(gc.Critere.Titre);
+                    dtgTxtCol.Binding = new Binding(titre);
                     dtgFacture.Columns.Add(dtgTxtCol);
+
                 }
             }
+        }
+
+        static string RemoveDiacritics(string text)
+        {
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in normalizedString)
+            {
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
         }
     }
 }
