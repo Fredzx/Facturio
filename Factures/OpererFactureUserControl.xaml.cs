24fcb8dd0c34a3a64ab2457917925104154f8460
bazzou789 <bazzou789@gmail.com>
2017-11-27 19:25:22 -0500

    On peut ajouter des produits à la facture, même ceux avec un critère libre, il faut corriger le bug de l'ajout du 2e produit

--- Factures/OpererFactureUserControl.xaml.cs
+++ Factures/OpererFactureUserControl.xaml.cs
@@ -1,14 +1,18 @@
-﻿using System.Globalization;
+﻿using Facturio.GabaritsCriteres;
+using System.Collections;
+using System.Globalization;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Forms;
 
 namespace Facturio.Factures
 {
     /// <summary>
     /// Interaction logic for OpererFactureUserControl.xaml
     /// </summary>
-    public partial class OpererFactureUserControl : UserControl
+    public partial class OpererFactureUserControl : System.Windows.Controls.UserControl
     {
         public OpererFactureUserControl()
         {
@@ -18,17 +22,58 @@ namespace Facturio.Factures
 
         private void CreerFacture()
         {
+            bool contientCritereLibre = false;
             foreach (var gc in OpererFacture.Gabarit.GabaritCriteres)
             {
                 if (gc.EstUtilise)
                 {
-                    var dtgTxtCol = new DataGridTextColumn { Header = gc.Critere.Titre};
-                    var titre = RemoveDiacritics(gc.Critere.Titre);
-                    dtgTxtCol.Binding = new Binding(titre);
-                    dtgFacture.Columns.Add(dtgTxtCol);
+                    var titre = GenererNomBinding(gc);
+                    if (titre != "")
+                    {
+                        var dtgTxtCol = new DataGridTextColumn { Header = gc.Critere.Titre };
+                        dtgTxtCol.Binding = new System.Windows.Data.Binding(titre);
+                        dtgTxtCol.IsReadOnly = true;
+                        dtgFacture.Columns.Add(dtgTxtCol);
+                    }
+                    else
+                    {
+                        contientCritereLibre = true;
+                        var col = new DataGridTemplateColumn();
+                        switch (gc.Critere.TypeCritere.TypeDuCritere)
+                        {
+                            case "string":
+                                //var col = new DataGridTextBoxColumn();
+                                FrameworkElementFactory textFactory = new FrameworkElementFactory(typeof(System.Windows.Controls.TextBox));
+                                DataTemplate textTemplate = new DataTemplate();
+                                textTemplate.VisualTree = textFactory;
+                                col.Header = gc.Critere.Titre;
+                                col.IsReadOnly = false;
+                                col.CellTemplate = textTemplate;
+                                col.CellEditingTemplate = textTemplate;
+                                dtgFacture.Columns.Add(col);
+                                break;
+                            case "bool":
+                                var col1 = new DataGridCheckBoxColumn();
+                                col1.Header = gc.Critere.Titre;
+                                col1.IsReadOnly = false;
+                                dtgFacture.Columns.Add(col1);
+                                break;
+                            case "float":
+                                // TODO: Validation de float (REGEX)
+                                break;
+                            case "int":
+                                // TODO: Validation de int (REGEX)
+                                break;
 
+                            default:
+                                break;
+                        }
+
+                    }
                 }
             }
+            if(contientCritereLibre)
+                System.Windows.MessageBox.Show("Ce gabarit contient un critère libre\nVeuillez entrer les informations par vous-même");
         }
 
         static string RemoveDiacritics(string text)
@@ -46,6 +91,56 @@ namespace Facturio.Factures
             }
 
             return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        private string GenererNomBinding(GabaritCritere gc)
+        {
+            string critere = "";
+            switch (gc.Critere.Titre)
+            {
+                case "Quantité":
+                    critere = "Quantite";
+                    break;
+                case "Prix":
+                    critere = "Produit.Prix";
+                    break;
+                case "Nom produit":
+                    critere = "Produit.Nom";
+                    break;
+                case "Description":
+                    critere = "Produit.Description";
+                    break;
+                case "":
+                    critere = "";
+                    break;
+                default:
+                    critere = "";
+                    break;
+            }
+            return critere;
+        }
+
+        private IEnumerable DeterminerCritereLibre(GabaritCritere gc)
+        {
+            //System.Console.WriteLine(gc.Critere.TypeCritere.TypeDuCritere);
+            switch (gc.Critere.TypeCritere.TypeDuCritere)
+            {
+                case "string":
+                    yield return new DataGridTextColumn();
+                    break;
+                case "bool":
+                    yield return new DataGridCheckBoxColumn();
+                    break;
+                case "float":
+                    // TODO: Validation de float (REGEX)
+                    break;
+                case "int":
+                    // TODO: Validation de int (REGEX)
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
