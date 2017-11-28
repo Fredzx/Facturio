0da88fcc066543dfde57b4282fb531384282e694
Frédéric SARRAZIN <1305906@cstj.qc.ca>
2017-11-28 13:17:28 -0500

    Ajout du dossier GenerateurPdf

---
+++ GenerateurPdf/GenerateurPdf.cs
@@ -0,0 +1,15 @@
+﻿using iTextSharp.text;
+
+namespace Facturio.GenerateurPdf
+{
+    /// <summary>
+    /// Wrapper class qui permet de générer un PDF facilement
+    /// </summary>
+    public class GenerateurPdf
+    {
+        public GenerateurPdf()
+        {
+
+        }
+    }
+}
